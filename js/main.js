8981d734a1b9743d7f955a27c536b3af2dba617e
Maksim Zozulya <soundmax15041987@gmail.com>
2024-06-20 17:01:36 +0300

    add new section portfolio and created slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+  slidesPerView: 1,
 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
